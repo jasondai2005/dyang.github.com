0d9a9d98fa7a29c0de2978859fd7f17ae28bbbb9
Derek Yang <yanghada@gmail.com>
2009-02-28 14:25:01 +0800

    Adding a ruby script to automatically generate blog template for me :-)

---
+++ scripts/gen.rb
@@ -0,0 +1,29 @@
+if ARGV.length == 0
+  p 'please specify the name of your blog. e.g. write-a-ruby-script'
+else
+  name = Date.today.to_s + '-' + ARGV.to_s + '.textile'
+  name_with_path = '../_posts/' + name
+  template = [
+    '---',
+    'layout: post',
+    'title: ',
+    '---',
+    '',
+    'h1. {{ page.title }}',
+    '',
+    'p(meta). ' + Date.today.to_s,
+    '',
+    '',
+    '',
+    '<pre class="terminal"><code></code></pre>',
+    '<pre><code></code></pre>',
+    '',
+    '']
+
+  blog = File.new(name_with_path, 'w')
+  template.each { |line| blog.puts line }
+  
+  p 'blog created at ' + name_with_path
+end
+
+
